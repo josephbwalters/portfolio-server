5e9fa7422cf4097f6de96793793b6df302f1bc34
Joseph Walters <joseph.walters@hey.com>
2021-01-31 14:31:44 -0500

    feat(graphql): add graphql

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  # GraphQL
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  end
+  post "/graphql", to: "graphql#execute"
+
+  # Swagger Docs
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
 
+  # REST Resources 
   namespace :api do
     namespace :v1 do
       resources :users
