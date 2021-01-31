921b6d4ffe1dd2c6abd7d6b81b5c194b755a5cdf
Joseph Walters <joseph.walters@hey.com>
2021-01-31 13:37:40 -0500

    feat(apis): add swagger docs, add api version

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  resources :users
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
+
+  namespace :api do
+    namespace :v1 do
+      resources :users
+    end
+  end
 
   get 'user_sessions/create'
   get 'user_sessions/destroy'
